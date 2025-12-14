12a6dfec8850f17df6d60df9c11175ef55534430
belljun3395 <195850@jnu.ac.kr>
2025-12-14 23:36:59 +0900

    refactor: improve readability of user assertions in GetUserByIdUseCaseTest

--- okchat-domain/okchat-domain-user/src/test/kotlin/com/okestro/okchat/user/application/GetUserByIdUseCaseTest.kt
+++ okchat-domain/okchat-domain-user/src/test/kotlin/com/okestro/okchat/user/application/GetUserByIdUseCaseTest.kt
@@ -36,9 +36,10 @@ class GetUserByIdUseCaseTest : BehaviorSpec({
             val result = useCase.execute(GetUserByIdUseCaseIn(userId = userId))
 
             then("활성화된 사용자 정보가 반환된다") {
-                result.user.shouldNotBeNull()
-                result.user.id shouldBe userId
-                result.user.active shouldBe true
+                val user = result.user
+                user.shouldNotBeNull()
+                user.id shouldBe userId
+                user.active shouldBe true
                 verify(exactly = 1) { userRepository.findByIdAndActive(userId, true) }
             }
         }
